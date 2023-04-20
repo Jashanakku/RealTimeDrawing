3c8c921d02616241b2b6297f5899e23a49f2261d
Jashanpreet Singh <jashanpreetsinghakku.com>
2023-04-20 23:00:25 +0530

    Setup the basic config of Socket in Client.

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
+const { socket } = require("engine.io");
 const express = require("express");
 const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 const port =3000;
 
+
+//io is like a channel perssonal connection with each other
+//so we need a broadcast connection.
+
 //connect with index.html in public folder
 app.use("/",express.static(__dirname + "/public"));
 
-// app.get("/",(req,res)=>{
-//     res.json("Hello Everyone!");
-// })
+const onConnection = socket =>{
+    socket.on("drawing",data => socket.broadcast.emit("drawing",data));     //broadcasting the data inside the drawing channel.
+}
 
-app.listen(port,()=>{
+io.on("connection",onConnection);     //on means any event happens
+
+http.listen(port,()=>{
     console.log("Server started in port " + port +".");
 })
--- public/script.js
+++ public/script.js
@@ -1,1 +1,14 @@
   
+//canvas working
+
+//Initialize Socket and get context of Canvas.
+var socket = io();
+var canvas = document.querySelector(".whiteboard");
+var context = canvas.getContext("2d");
+
+
+//Configuration 
+var drawing = false;
+var current ={
+    color:"black"
+}
