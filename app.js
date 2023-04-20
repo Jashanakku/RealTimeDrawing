8c82e4e0842a16633e210fe1017d5a42621609f0
Jashanpreet Singh <jashanpreetsinghakku.com>
2023-04-20 21:41:17 +0530

    Included HTTP Server and connected with IO.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 const port =3000;
 
 //connect with index.html in public folder
